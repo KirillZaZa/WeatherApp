25351967b28ec846e3ce8291f3483e9608c42bf1
kiril <kirill.189452@gmail.com>
2021-10-04 23:04:09 +0300

    updated gitignore

--- app/src/main/java/com/example/weatherapp/data/network/Constants.kt
+++ app/src/main/java/com/example/weatherapp/data/network/Constants.kt
@@ -4,7 +4,7 @@ import com.example.weatherapp.R
 
 
 object BuildConfig{
-    private const val API_KEY = R.string.weather_key
+    private const val API_KEY = R.string.open_weather_api_key
 
     const val API_URL = "pro.openweathermap.org/data/2.5/forecast/hourly?q={cnt=24}&appid={$API_KEY}"
 }
